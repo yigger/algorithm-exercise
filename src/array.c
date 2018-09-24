d9fd8b96f90a348f5b163d24e57a2498e68021c4
young <21738331@qq.com>
2018-09-24 22:18:39 +0800

    add array comment

--- src/array.c
+++ src/array.c
@@ -8,15 +8,23 @@
 
 static enum STATE expendArray(Array *array);
 
+/**
+ * 创建新的数组 
+ * @param[out] 指向数组的指针的指针
+ * @return OK || MALLOC_ERR 返回成功或者内存分配失败的状态码 
+*/
 enum STATE createArray(Array **out) {
     Array *array;
     array = zmalloc(sizeof(array));
     if (!array) {
         return MALLOC_ERR;
     }
+
     array->len = DEFAULT_LEN;
     array->used = 0;
+    // 分配存储元素的数组长度，默认值为 DEFAULT_LEN
     array->items = zmalloc(sizeof(void*) * array->len);
+    // 分配失败则销毁后退出
     if (!array->items) {
         zfree(array);
         return MALLOC_ERR;
@@ -25,6 +33,12 @@ enum STATE createArray(Array **out) {
     return OK;
 }
 
+/**
+ * 往数组追加一个元素，如果数组已用长度超过 60%，则增加数组容量
+ * @params[array] 数组指针
+ * @params[item] 无类型的地址
+ * @return OK || ERROR
+*/
 enum STATE arrayPush(Array *array, void *item) {
     if (check_expend(array) == 1) {
         if (expendArray(array) != OK) {
@@ -37,8 +51,14 @@ enum STATE arrayPush(Array *array, void *item) {
     return OK;
 }
 
+/**
+ * 追加数组的容量， 每次默认增加 DEFAULT_LEN 的长度
+ * @params[array] 数组指针
+ * @return OK || ERROR || MALLOC_ERR
+*/
 static enum STATE expendArray(Array *array) {
     array->len += DEFAULT_LEN;
+    // 长度不能超过最大限度
     if (array->len > MAX_ELEMENTS_LENGTH) {
         return ERROR; 
     }
@@ -49,6 +69,7 @@ static enum STATE expendArray(Array *array) {
         return MALLOC_ERR;
     }
 
+    // 将存储在 array 的旧元素全部迁移到扩容后的数组中去，然后销毁旧的内存空间
     memcpy(newItems, array->items, sizeof(void *) * array->len);
     zfree(array->items);
 
@@ -56,6 +77,10 @@ static enum STATE expendArray(Array *array) {
     return OK;
 }
 
+/**
+ * 销毁数组
+ * @params[array] 数组指针 
+*/
 void destroyArray(Array *array) {
     zfree(array->items);
     zfree(array);
